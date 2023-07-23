a077fcf762e38f03a2694387b4cfe531881d3855
your name <abc@gmail.com>
2023-07-23 17:08:45 +0700

    format and clean code & rename file user --> account

---
+++ src/routes/manager.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const authController = require("../app/controllers/auth");
+const productController = require("../app/controllers/productController");
+const StaticData = require("../utils/StaticData");
+const router = express.Router();
+
+router
+  .route("/")
+  .get(
+    authController.protect,
+    authController.restricTo(
+      StaticData.AUTH.Role.admin,
+      StaticData.AUTH.Role.manager
+    ),
+    (req, res) => {
+      res.render("manager", {
+        title: "manager",
+        linkcss: "/css/manager.css",
+        linkjs: "/js/manager.js",
+      });
+    }
+  );
+
+router.route("/Product").get(
+  authController.protect,
+  authController.restricTo(
+    StaticData.AUTH.Role.admin,
+    StaticData.AUTH.Role.manager
+  ),
+  (req, res) => {
+    res.render("manager", {
+      title: "manager",
+      linkcss: "/css/manager.css",
+      linkjs: "/js/manager.js",
+      product: true,
+    });
+  },
+  productController.getALLProductsHandler
+);
+
+module.exports = router;
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,28 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("./../app/controllers/productController");
-const authController = require("../app/controllers/auth");
-const StaticData = require("../utils/StaticData");
 
 router.param("id", productController.checkProductById);
 
-// router
-//   .route("/abc")
-//   .post(
-//     authController.protect,
-//     authController.restricTo(
-//       StaticData.AUTH.Role.admin,
-//       StaticData.AUTH.Role.manager
-//     ),
-//     productController.createProductHandler
-//   );
-// router
-//   .route("/:id")
-//   .get(
-//     authController.protect,
-//     authController.restricTo(StaticData.AUTH.Role.admin),
-//     productController.getProductHandler
-//   );
 router.route("/Laptops").get(productController.showLaptopHandler);
 router.route("/Phones").get(productController.showPhoneHandler);
 router
