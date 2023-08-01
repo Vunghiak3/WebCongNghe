02c95ce38046a9e4d7402dcc539ddcae82852dee
Sewlz <tnam7232@gmail.com>
2023-08-01 14:45:05 +0700

    add payment in appjs

--- src/app.js
+++ src/app.js
@@ -6,8 +6,105 @@ const handlebars = require("express-handlebars");
 const session = require("express-session");
 const methodOverride = require("method-override");
 // const authController = require("./app/controllers/auth");
-const paypal = require("./payment")
 
+//start of paypal test
+const paypal = require("paypal-rest-sdk");
+
+// Configure PayPal
+paypal.configure({
+  mode: "sandbox", // sandbox or live
+  client_id:
+    "AQbgZAurnwqO06DdEoSDbkD_f-YL1fX5NGxGL0eY3rmnhoiu-sipsjNsq4oivA0ezRdO7uBvKdVjcP1h",
+  client_secret:
+    "EN8z0dhpLkN_8eFRnWJY-6VmPqaCddxDGh6v0Vbc8a3_qxHjekjQyn91WEeEwmrHmtc6vKwyly4nTQ8R",
+});
+
+// Middleware to parse JSON and form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// PayPal payment route
+app.post("/pay", (req, res) => {
+  const create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal",
+    },
+    redirect_urls: {
+      return_url: "http://localhost:8080/Home",
+      cancel_url: "http://localhost:8080",
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: "Test Item",
+              sku: "ITEM001",
+              price: "10.00",
+              currency: "USD",
+              quantity: 1,
+            },
+          ],
+        },
+        amount: {
+          currency: "USD",
+          total: "10.00",
+        },
+        description: "Test Payment for Node.js",
+      },
+    ],
+  };
+
+  paypal.payment.create(create_payment_json, (error, payment) => {
+    if (error) {
+      throw error;
+    } else {
+      for (let i = 0; i < payment.links.length; i++) {
+        if (payment.links[i].rel === "approval_url") {
+          res.redirect(payment.links[i].href);
+        }
+      }
+    }
+  });
+});
+
+// Success route after payment completion
+app.get("/success", (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    payer_id: payerId,
+    transactions: [
+      {
+        amount: {
+          currency: "USD",
+          total: "10.00",
+        },
+      },
+    ],
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, (error, payment) => {
+    if (error) {
+      console.log(error.response);
+      throw error;
+    } else {
+      res.render("success", {
+        title: "Payment Successful",
+        transactionId: payment.id,
+      });
+    }
+  });
+});
+
+// Cancel route if the user cancels the payment
+app.get("/cancel", (req, res) => {
+  res.render("cancel", { title: "Payment Cancelled" });
+});
+
+//end of paypal test
 app.use(express.json());
 //HTTP logger
 app.use(morgan("dev"));
